c545d0271b3cb97014c7ce666cdb3913a2a29f23
julianadenauer <github@julianadenauer.de>
2014-08-13 15:29:58 +0200

    fee and registration link for each workshop

--- workshops.php
+++ workshops.php
@@ -29,7 +29,7 @@
 					<div class="description col-sm-8 col-sm-offset-2 col-xs-12 col-xs-offset-0" style="padding-bottom:100px;">
 						<h2>prototyping interfaces</h2>					
 						<h3>Let’s get physical with vvvv & draw with your body</h3>
-						<h4>by Jochen Leinberger and Mark Lukas<br/>1 day | 26.09.2014, 10:00 – 16:00</h4>
+						<h4>by Jochen Leinberger and Mark Lukas<br/>1 day | 26.09.2014, 10:00 – 16:00<br/>30€ (student: 24€) – <a href="http://retune.de/2014/tickets#tito-register-form">SIGN UP</a></h4>
 						<p>This workshop will be inspired by the book “Prototyping Interfaces – interactive sketching with vvvv”, in which we explore the method of doing, testing and iterating to explore new ideas and concepts with functional prototypes. Because we think best way to invent the future is to prototype it! We will show you how to explore your interface idea by building it up and programming it in a rapid way.</p>
 						<p>In the beginning we will show you the basic workflow of how to use vvvv and connect it to different sensors like the accelerometer of your mobile phones or Arduino based sensors. Later we will show you how to use the sensor readings and generate and manipulate real-time graphics in vvvv. At the end you can control them with your body as an input.</p>
 						<p><em>Jochen Leinberger</em> studied industrial design at HfG Offenbach. He is really excited about developing and inventing new custom-made technology which is focused on solving problems, improving life and making the impossible possible. After working 7 years at MESO Digital Interiors as conceptual designer for physical interaction, he started working as UX Designer and for the conception of new products and services at T-Gallery, the Future Forum of Deutsche Telekom.</p>
@@ -72,7 +72,7 @@
 				<div class="workshop_info col-md-6 col-xs-12">
 					<div class="description col-sm-8 col-sm-offset-2 col-xs-12 col-xs-offset-0" style="padding-bottom:100px;">
 						<h2>vvvv</h2>					
-						<h4>by Anton and Joreg<br/>1 day | 26.09.2014, 10:00 – 16:00</h4>
+						<h4>by Anton and Joreg<br/>1 day | 26.09.2014, 10:00 – 16:00<br/>30€ (student: 24€) – <a href="http://retune.de/2014/tickets#tito-register-form">SIGN UP</a></h4>
 						<p>In this workshop we will give you an easy introduction to vvvv by showing you how to use and create GPU­-based image effects, mixers and animations.</p>
 						<p>Ever dreamed to have the layered, masked and full­of­effects power of Photoshop or After Effects but in real­time? Are you doing live visual performances, creating animations, building interactive exhibits or just want to start using vvvv? This workshop is for you.</p>
 						<p>Chains of visual effects are easy to use for the novice, yet quite exhaustive in their possibilities.</p>
@@ -109,7 +109,7 @@
 				<div class="workshop_info col-md-6 col-xs-12">
 					<div class="description col-sm-8 col-sm-offset-2 col-xs-12 col-xs-offset-0" style="padding-bottom:100px;">
 						<h2>glemmer</h2>					
-						<h4>by Felix Hardmood Beck and Simon Schiessl<br/>1 day | 26.09.2014, 10:00 – 16:00</h4>
+						<h4>by Felix Hardmood Beck and Simon Schiessl<br/>1 day | 26.09.2014, 10:00 – 16:00<br/>55€ (including material, student: 44€) – <a href="http://retune.de/2014/tickets#tito-register-form">SIGN UP</a></h4>
 						<p>Glemmer is a small, battery powered programmable light module. There are 18446744073709551616 possibilities to light up 64 red glowing LEDs and even more possibilities to design something with it. An integrated microcontroller can be programmed via Arduino or Processing. Glemmer can be used for battery-powered New Media accessories, fashion gimmickry or small electronic intervention. For 24,99€ every participant will be supplied with hard- and software components to build their own take-away and out-of-the-box working Glemmer module.</p>
 						<p>Glemmer's size (width, length, height): 30.2 mm x 30.2 mm x 13.7 mm</p>
 						<p>Glemmer kit comes with:<br/>
@@ -148,7 +148,7 @@
 				<div class="workshop_info col-md-6 col-xs-12">
 					<div class="description col-sm-8 col-sm-offset-2 col-xs-12 col-xs-offset-0" style="padding-bottom:100px;">
 						<h2>Introduction to openFrameworks</h2>					
-						<h4>by Arturo Castro<br/>1 day | 26.09.2014, 10:00 – 16:00</h4>
+						<h4>by Arturo Castro<br/>1 day | 26.09.2014, 10:00 – 16:00<br/>30€ (student: 24€) – <a href="http://retune.de/2014/tickets#tito-register-form">SIGN UP</a></h4>
 						<p>openFrameworks is a creative coding toolkit for artists and designers that allows to work with graphics, sound, video, computer vision and many others.</p>
 						<p>During the workshop we'll adquire some basic knowledge that will help us program using openFrameworks. The workshop is oriented to people with no or few experience in openFrameworks but some experience with programming (Processing, php, javascript...).</p>
 						<p><em>Arturo Castro</em> is an artist, educator and engineer currently based in Berlin. He is one of the core developers of the open source toolkit for artists and designers openFrameworks. Currently he works on his own and collaborates with other artists and technologists on projects usually in the field of interactive installations.<br/>His main interests are related with open source culture in the context of artistic practices and technology literacy and his work has been exhibited in museums like Maison d'Ailleurs in Switzerland, the London Design Museum or Moscow's Multimedia Art Museum among others.</p>
@@ -186,7 +186,7 @@
 				<div class="workshop_info col-md-6 col-xs-12">
 					<div class="description col-sm-8 col-sm-offset-2 col-xs-12 col-xs-offset-0" style="padding-bottom:100px;">
 						<h2>3d scanning and printing</h2>					
-						<h4>by Bram de Vries<br/>1 day | 26.09.2014, 10:00 - 16:00</h4>
+						<h4>by Bram de Vries<br/>1 day | 26.09.2014, 10:00 - 16:00<br/>30€ (student: 24€) – <a href="http://retune.de/2014/tickets#tito-register-form">SIGN UP</a></h4>
 						<p>In the workshop you will 3D-Scan your head, prepare it for printing and print it.</p>
 						<p>Scanning yourself with a Kinect camera is a fun process. The 3D model that comes out of it needs some repairing and can also be modified and remixed with other objects. After that you can print a small version your head on one of the i3 Berlin 3D printers. This practical workshop gives you an overview of the possibilities of 3D printing and scanning.</p>
 						<p><a href="http://www.laydrop.com/" target="_blank">Laydrop</a> develops open-source 3D printing solutions like the i3 Berlin.
@@ -216,7 +216,7 @@
 					<div class="description col-sm-8 col-sm-offset-2 col-xs-12 col-xs-offset-0" style="padding-bottom:100px;">
 						<h2>The superobjects Paradigm</h2>					
 						<h3>Rebooting Programming as a Creative Practice</h3>
-						<h4>by Alan N. Shapiro<br/>1 day | 26.09.2014, 10:00 – 16:00</h4>
+						<h4>by Alan N. Shapiro<br/>1 day | 26.09.2014, 10:00 – 16:00<br/>30€ (student: 24€) – <a href="http://retune.de/2014/tickets#tito-register-form">SIGN UP</a></h4>
 						<p>This workshop introduces the superobjects coding project. The first immediate goal of my work to change the profile of who programmers are. To get away from the idea that programmers are mathematical-logical-engineering types and transform coding into a creative art form. There was a major paradigm shift in programming abouts three decade ago from the procedural paradigm to the object-oriented paradigm. What I am arguing for is a paradigm shift to a third paradigm, partly based on a further extrapolation of the shift from the first to the second paradigms. Programming so far has consisted of sending instructions to a machine or dead thing, basing software on the metaphor of the mechanistic machine. In the superobjects paradigm, software is considered semi-alive, based on the metaphor of the living organism as an information system. Code will be poetic. Existing programming is based on the logic of the discrete. Crucial in the new programming is a logic of similarities or resonances. We will also discuss the first practical applications of the superobjects paradigm in the conversational flow area of Spoken Dialog Technology and in software for dance choreography.</p>
 						<p><em>Alan N. Shapiro</em> is a trans-disciplinary thinker who studied science-technology at MIT and philosophy-history-literature at Cornell University. He is the author or editor of three books: Star Trek: Technologies of Disappearance, Software of the Future, and The Technological Herbarium. He is senior lecturer in informatics and media theory at the Art and Design University, Offenbach.</p>
 						<div class="workshop-meta">
@@ -253,7 +253,7 @@
 					<div class="description col-sm-8 col-sm-offset-2 col-xs-12 col-xs-offset-0" style="padding-bottom:100px;">
 						<h2>NETworkshop</h2>
 						<h3>Lightning in the Age of Cloud-Computing</h3>
-						<h4>by Julian Oliver and Danja Vasiliev<br/>5 days | 22. – 26.09.2014</h4>
+						<h4>by Julian Oliver and Danja Vasiliev<br/>5 days | 22. – 26.09.2014<br/>250€ (student: 200€) – <a href="http://retune.de/2014/tickets#tito-register-form">SIGN UP</a></h4>
 						<p>Ask anyone how the postal system works and they would give a vaguely correct description. Few however would come close to describing how email, let alone a computer network itself, actually functions. With this lack of knowledge comes a risk; we lack the practical understanding to effectively read the infrastructural and political implications of our increased dependency on this technology.<br/>
 						In this intense 5 day workshop Oliver and Vasiliev teach low level networking using only command line tools and networking hardware. In doing so, students learn not only how to create and manipulate computer networks, but also how they can be used to manipulate us.<br/>
 						No prior knowledge of computer networking, programming or command line interaction is required.</p>
