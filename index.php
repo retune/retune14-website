a8d667c3a48e45800478bbd71b625755d9da07d1
julianadenauer <github@julianadenauer.de>
2014-08-18 11:33:08 +0200

    open call link entfernt

--- index.php
+++ index.php
@@ -39,8 +39,6 @@
 				</div>
 			</div>
 
-			<a href="http://bit.ly/retune14-opencall-website" target="_blank"><img src="img/call-announcement.png" style="position: absolute; top: 55px; right: 60px;" /></a>
-
 		</div> <!-- /container -->
 
 <?php require_once('footer.php'); ?> 
