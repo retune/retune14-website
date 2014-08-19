9db39f2d21b57c13ac9f9b2d89ec3b62d37d360d
julianadenauer <github@julianadenauer.de>
2014-08-19 15:35:54 +0200

    added alt-attributes to supporters

--- index.php
+++ index.php
@@ -16,26 +16,26 @@
 			<div class="row" id="supporter">
 				<div class="col-sm-6 text-center">
 					<h1>sponsors</h1>
-					<a href="http://mailchimp.com" target="_blank"><img src="img/mailchimp.png" /></a><br/>
-					<a href="http://wetransfer.com" target="_blank"><img src="img/wetransfer.png" /></a><br/>
-					<a href="http://cloud-film.tv" target="_blank"><img src="img/cloud.png" /></a>
+					<a href="http://mailchimp.com" target="_blank"><img src="img/mailchimp.png" alt="mailchimp"/></a><br/>
+					<a href="http://wetransfer.com" target="_blank"><img src="img/wetransfer.png" alt="wetransfer"/></a><br/>
+					<a href="http://cloud-film.tv" target="_blank"><img src="img/cloud.png" alt="cloud" /></a>
 					
 					<h1>technical support</h1>
-					<a href="http://serve-u.de" target="_blank"><img src="img/serve-u_color.png" /></a>
+					<a href="http://serve-u.de" target="_blank"><img src="img/serve-u_color.png" alt="serve-u"/></a>
 				</div>
 
 				<div class="col-sm-6 text-center">
 					<h1>friends and supporters</h1>
-					<a href="http://sonicedevelopment.com" target="_blank"><img src="img/sonice.png" width="220px" /></a><br/>
-					<a href="http://studio-regular.com/" target="_blank"><img src="img/StudioRegular.png" width="220px" /></a><br/>
-					<a href="http://theconstitute.org/" target="_blank"><img src="img/the_constitute.png" /></a><br/>
-					<a href="http://tinkersoup.de/" target="_blank"><img src="img/tinkersoup_color_small.png" /></a><br/>
-					<a href="http://formulor.de/" alt="formulor lasercut lasercutting" target="_blank"><img src="img/formulor.png" /></a>
+					<a href="http://sonicedevelopment.com" target="_blank"><img src="img/sonice.png" width="220px" alt="sonice development" /></a><br/>
+					<a href="http://studio-regular.com/" target="_blank"><img src="img/StudioRegular.png" width="220px" alt="studio regular grafikdesign" /></a><br/>
+					<a href="http://theconstitute.org/" target="_blank"><img src="img/the_constitute.png" alt="the constitute" /></a><br/>
+					<a href="http://tinkersoup.de/" target="_blank"><img src="img/tinkersoup_color_small.png" alt="tinkersoup" /></a><br/>
+					<a href="http://formulor.de/" target="_blank"><img src="img/formulor.png" alt="formulor lasercut lasercutting"/></a>
 
 					<h1>partner events</h1>
-					<a href="http://fiber-space.nl/" target="_blank"><img src="img/FIBER.png" /></a><br/>
-					<a href="http://kikk.be" target="_blank"><img src="img/kikk.png" /></a>
-					<a href="http://www.alpha-ville.co.uk" target="_blank"><img src="img/alphaville.png" style="margin-left:30px"/></a>
+					<a href="http://fiber-space.nl/" target="_blank"><img src="img/FIBER.png" alt="fiber festival" /></a><br/>
+					<a href="http://kikk.be" target="_blank"><img src="img/kikk.png"  alt="kikk festival" /></a>
+					<a href="http://www.alpha-ville.co.uk" target="_blank"><img src="img/alphaville.png" style="margin-left:30px" alt="alpha ville"/></a>
 				</div>
 			</div>
 
