ef8c0d0c1e6e0eafe923f413e2dd7388cfc040fe
julianadenauer <github@julianadenauer.de>
2014-09-19 00:48:02 +0200

    full schedule as draft online

---
+++ schedule2.php
@@ -0,0 +1,272 @@
+<?php require_once('head.php'); ?> 
+	
+    	<div class="container text-page" style="background-color: white; padding-bottom: 200px" id="schedule">				
+	    	<div class="row">
+	    		<h1>schedule</h1>
+				<div class="col-md-4">
+					<h2>Friday &ndash; 26.09.2014</h2>
+					<table style="width: 100%;">
+						<tr>
+							<th width="20%"></th>
+							<th width="40%">main Stage</td>
+							<th>second stage</th>
+						</tr>
+
+						<tr height="700" class="main-block">
+							<td class="time">10:00</td>
+							<td class="speaker" colspan="2">Workshops
+								<p>
+									Prototyping Interfaces<br/>
+									vvvv<br/>
+									Glemmer<br/>
+									Openframeworks<br/>
+									3D Scanning and Printing<br/>
+									Superobjects Paradigm<br/>
+									Twitter Orchestra<br/>
+									beanotherlab
+								</p>
+							</td>
+						</tr>
+						
+						<tr height="100" class="main-block">
+							<td class="time">17:00</td>
+							<td colspan="2" class="general">Registration</td>
+						</tr>
+						<tr height="50" class="main-block">
+							<td class="time">18:00</td>
+							<td class="speaker">Welcome & Introduction</td>
+							<td></td>
+						</tr>
+						<tr height="50">
+							<td></td>
+							<td class="speaker">Alice Wang</td>
+							<td></td>
+						</tr>
+						<tr height="50">
+							<td></td>
+							<td class="speaker">United Visual Artists</td>
+							<td></td>
+						</tr>
+
+						<tr height="400" class="main-block">
+							<td class="time">19:30</td>
+							<td colspan="2" style="height:80px" class="evening">Get Together
+								<p>Music + Drinks</p>
+								<p><em>Presentation</em><br/>
+									Choreographic Coding Lab<br/><br/>
+								<em>Performance</em></br>
+								VJ workshop<br/><br/>
+								<em>Performance</em><br/>
+								Moritz Simon Geist
+								</p>
+								<p><em>DJ</em><br/>The Constitute</p>
+
+							</td>	
+						</tr>
+					</table>
+					
+				</div>
+
+				<div class="col-md-4" style="padding-bottom: 200px">
+					<h2>Saturday &ndash; 27.09.2014</h2>
+					<table style="width: 100%;">
+						<tr>
+							<th width="20%"></th>
+							<th width="40%">main stage</td>
+							<th>second stage</th>
+						</tr>
+
+						<tr height="50" class="main-block">
+							<td class="time">10:00</td>
+							<td class="general" colspan="2">Registration</td>
+							<td></td>
+						</tr>
+
+						<tr height="66" class="main-block">
+							<td class="time">10:30</td>
+							<td class="speaker">Filip Visnjic</td>
+							<td></td>
+						</tr>
+						<tr height="66">
+							<td></td>
+							<td class="speaker">Domenico Quaranta<br/></td>
+							<td></td>
+						</tr>
+						<tr height="66">
+							<td></td>
+							<td class="speaker">Wolf Lieser</td>
+							<td></td>
+						</tr>
+						<tr height="50" class="main-block">
+							<td class="time">12:30</td>
+							<td></td>
+							<td class="discussion">Discussion 1</td>
+						</tr>
+						<tr height="100" class="main-block">
+							<td class="time">13:00</td>
+							<td colspan="2" class="general">Lunch Break</td>
+						</tr>
+
+						<tr height="50" class="main-block">
+							<td class="time">14:00</td>
+							<td class="speaker">Jarii van Gohl</td>
+							<td></td>
+						</tr>
+						<tr height="50">
+							<td></td>
+							<td class="speaker">Gabriel Shalom</td>
+							<td></td>
+						</tr>
+						<tr height="50">
+							<td></td>
+							<td class="speaker">Moritz Simon Geist</td>
+							<td></td>
+						</tr>
+						<tr height="50">
+							<td></td>
+							<td class="speaker">Christine Sun Kim</td>
+							<td></td>
+						</tr>
+						<tr height="50" class="main-block">
+							<td class="time">16:00</td>
+							<td></td>
+							<td class="discussion">Discussion 2</td>
+						</tr>
+						<tr height="50" class="main-block">
+							<td class="time">16:30</td>
+							<td class="speaker">A Parede</td>
+							<td></td>
+						</tr>
+						<tr height="50">
+							<td></td>
+							<td class="speaker">Vincent Dubois</td>
+							<td></td>
+						</tr>
+						<tr height="50">
+							<td></td>
+							<td class="speaker">Zach Blas</td>
+							<td></td>
+						</tr>
+						<tr height="50">
+							<td></td>
+							<td class="speaker">Jamie Allen</td>
+							<td></td>
+						</tr>
+						<tr height="50" class="main-block">
+							<td class="time">18:30</td>
+							<td></td>
+							<td class="discussion">Discussion 3</td>
+						</tr>
+						<tr height="50">
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+						<tr height="50">
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+
+						<tr height="200" class="main-block">
+							<td class="time">20:00</td>
+							<td colspan="2" style="height:80px" class="evening">Sound Performances
+								<p>@ LEAP</br>Leipziger Str. 63<br/>10117 Berlin</p>
+							</td>
+						</tr>
+						<tr height="200" class="main-block">
+							<td class="time">22:00</td>
+							<td colspan="2" style="height:80px" class="afterparty">Afterparty
+								<p>@ LEAP</br>Leipziger Str. 63<br/>10117 Berlin</p>
+							</td>
+						</tr>
+					</table>
+				</div>
+
+				<div class="col-md-4">
+					<h2>Sunday &ndash; 28.09.2014</h2>
+					<table style="width: 100%;">
+						<tr>
+							<th width="20%"></th>
+							<th width="40%">main Stage</td>
+							<th>second stage</th>
+						</tr>
+
+						<tr height="50">
+							<td class="time">10:30</td>
+							<td class="speaker">Susa Schmid</td>
+							<td></td>
+						</tr>
+						<tr height="50">
+							<td></td>
+							<td class="speaker">Cyborgs e.V.<br/></td>
+							<td></td>
+						</tr>
+						<tr height="50">
+							<td></td>
+							<td class="speaker">BeAnotherLab</td>
+							<td></td>
+						</tr>
+						<tr height="50">
+							<td class="time">12:00</td>
+							<td></td>
+							<td class="discussion">Discussion 4</td>
+						</tr>
+						<tr height="50">
+							<td></td>
+							<td class="speaker">Stefano Mirti</td>
+							<td class="company">job corner</td>
+						</tr>
+						<tr height="100">
+							<td class="time">13:00</td>
+							<td colspan="2" class="general">Lunch Break</td>
+						</tr>
+
+						<tr height="50">
+							<td class="time">14:00</td>
+							<td class="speaker">Anaisa Franco</td>
+							<td></td>
+						</tr>
+						<tr height="50">
+							<td></td>
+							<td class="speaker">Robin Parmar</td>
+							<td></td>
+						</tr>
+						<tr height="50">
+							<td></td>
+							<td class="speaker">Karina Smigla-Bobinski</td>
+							<td></td>
+						</tr>
+						<tr height="50">
+							<td class="time">15:30</td>
+							<td></td>
+							<td class="discussion">Discussion 5</td>
+						</tr>
+						<tr height="50">
+							<td class="time">16:00</td>
+							<td class="speaker">Karsten Schmidt</td>
+							<td></td>
+						</tr>
+						<tr height="50">
+							<td></td>
+							<td class="speaker">Joachim Sauter</td>
+							<td></td>
+						</tr>
+						<tr height="50">
+							<td></td>
+							<td class="speaker">Random International</td>
+							<td></td>
+						</tr>
+						<tr height="50">
+							<td class="time">18:30</td>
+							<td></td>
+							<td class="afterparty">Feedback & Goodbye</td>
+						</tr>
+						
+					</table>
+				</div>				
+			</div> <!-- /row -->
+
+		</div> <!-- /container -->
+
+<?php require_once('footer.php'); ?> 
