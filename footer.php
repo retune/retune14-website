e649fa25f9b6ad08b05dd7ad2b1a53cc290a13e8
Julian Adenauer <github@julianadenauer.de>
2016-01-19 13:57:18 +0100

    changed retune_conf to retuneberlin for twitter and instagram

--- footer.php
+++ footer.php
@@ -2,9 +2,9 @@
 			<p>retune 2014 // 26. – 28.09.2014, ARENA Glashaus Berlin</p>
 			<ul>
 				<li><a href="http://eepurl.com/oAiGz" target="_blank">newsletter</a></li>
-				<li><a href="https://twitter.com/retune_conf" target="_blank">twitter</a></li>
+				<li><a href="https://twitter.com/retuneberlin" target="_blank">twitter</a></li>
 				<li><a href="https://www.facebook.com/retuneconference" target="_blank">facebook</a></li>
-				<li><a href="http://instagram.com/retune_conf" target="_blank">instagram</a></li>
+				<li><a href="http://instagram.com/retuneberlin" target="_blank">instagram</a></li>
 				<li><a href="http://flickr.com/retuneconference" target="_blank">flickr</a></li>
 				<li><span class="glyphicon glyphicon-minus"></span></li>
 				<li><a href="terms.php">terms</a></li>
